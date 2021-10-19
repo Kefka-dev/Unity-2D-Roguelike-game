3c668c16475136cfdaad28b59d540462cfc6462e
Kefka-dev <sith.patri@gmail.com>
2021-10-19 22:42:45 +0200

    player now can not move while exiting the level

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -23,6 +23,8 @@ public class PlayerController : MonoBehaviour
 
     public float dashSpeed = 8f, dashLenght = .5f, dashCooldown = 1f, dashInvincibility = .5f;
 
+    public bool canMove = true;
+
     private Vector2 moveInput;
     private Camera theCam;
     private float shotCounter;
@@ -45,95 +47,105 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        moveInput.x = Input.GetAxisRaw("Horizontal");
-        moveInput.y = Input.GetAxisRaw("Vertical");
-
-        moveInput.Normalize();
-        //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
-
-        theRB.velocity = moveInput * activeMovespeed;
-
-        Vector3 mousePos = Input.mousePosition;
-        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
-
-
-        //otocenie hraca a zbrane tak aby mierenie zbranou vyzeralo normalne
-        if (mousePos.x < screenPoint.x)
-        {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-            gunArm.transform.localScale = new Vector3(-0.75f, -0.75f, 1f);
-        }
-        else
-        {
-            transform.localScale = new Vector3(1f, 1f, 1f);
-            gunArm.transform.localScale = new Vector3(0.75f, 0.75f, 1f);
-        }
-
-        //otacanie ruk
-        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
-        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
-        gunArm.rotation = Quaternion.Euler(0, 0, angle);
-
-        shotCounter -= Time.deltaTime;
-
-        if (Input.GetMouseButtonDown(0) && shotCounter <= 0)
+        if (canMove)
         {
 
-            Instantiate(bullet, firePoint.position, firePoint.rotation);
-            AudioManager.instance.PlaySFX(13);
-            shotCounter = timeBetweenShots;
-        }
 
-        if (Input.GetMouseButton(0))
-        {
 
-            if (shotCounter <= 0)
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
+
+            moveInput.Normalize();
+            //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
+
+            theRB.velocity = moveInput * activeMovespeed;
+
+            Vector3 mousePos = Input.mousePosition;
+            Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+
+            //otocenie hraca a zbrane tak aby mierenie zbranou vyzeralo normalne
+            if (mousePos.x < screenPoint.x)
             {
+                transform.localScale = new Vector3(-1f, 1f, 1f);
+                gunArm.transform.localScale = new Vector3(-0.75f, -0.75f, 1f);
+            }
+            else
+            {
+                transform.localScale = new Vector3(1f, 1f, 1f);
+                gunArm.transform.localScale = new Vector3(0.75f, 0.75f, 1f);
+            }
+
+            //otacanie ruk
+            Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+            float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+            gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+            shotCounter -= Time.deltaTime;
+
+            if (Input.GetMouseButtonDown(0) && shotCounter <= 0)
+            {
+
                 Instantiate(bullet, firePoint.position, firePoint.rotation);
                 AudioManager.instance.PlaySFX(13);
                 shotCounter = timeBetweenShots;
             }
-        }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-
-            if (dashCooldownCounter <= 0 && dashDuration <= 0)
+            if (Input.GetMouseButton(0))
             {
-                AudioManager.instance.PlaySFX(9);
-                activeMovespeed = dashSpeed;
-                dashDuration = dashLenght;
 
-                PlayerHealthController.instance.BeInvincible(dashInvincibility);
-                anim.SetTrigger("dash");
+                if (shotCounter <= 0)
+                {
+                    Instantiate(bullet, firePoint.position, firePoint.rotation);
+                    AudioManager.instance.PlaySFX(13);
+                    shotCounter = timeBetweenShots;
+                }
             }
-        }
 
-        if (dashDuration > 0)
-        {
-            dashDuration -= Time.deltaTime;
-            if(dashDuration <= 0)
+            if (Input.GetKeyDown(KeyCode.Space))
             {
-                activeMovespeed = moveSpeed;
-                dashCooldownCounter = dashCooldown;
+
+                if (dashCooldownCounter <= 0 && dashDuration <= 0)
+                {
+                    AudioManager.instance.PlaySFX(9);
+                    activeMovespeed = dashSpeed;
+                    dashDuration = dashLenght;
+
+                    PlayerHealthController.instance.BeInvincible(dashInvincibility);
+                    anim.SetTrigger("dash");
+                }
             }
-        }
 
-        if (dashCooldownCounter > 0)
-        {
-            dashCooldownCounter -= Time.deltaTime;
-        }
+            if (dashDuration > 0)
+            {
+                dashDuration -= Time.deltaTime;
+                if (dashDuration <= 0)
+                {
+                    activeMovespeed = moveSpeed;
+                    dashCooldownCounter = dashCooldown;
+                }
+            }
+
+            if (dashCooldownCounter > 0)
+            {
+                dashCooldownCounter -= Time.deltaTime;
+            }
 
 
-        //zistovanie ci sa hrac hybe na to aby sme vedeli ktora animacia ma prebiehat 
-        if (moveInput != Vector2.zero)
-        {
-            anim.SetBool("isMoving", true);
+            //zistovanie ci sa hrac hybe na to aby sme vedeli ktora animacia ma prebiehat 
+            if (moveInput != Vector2.zero)
+            {
+                anim.SetBool("isMoving", true);
+            }
+            else
+            {
+                anim.SetBool("isMoving", false);
+            }
+
         } else
         {
+            theRB.velocity = Vector2.zero;
             anim.SetBool("isMoving", false);
         }
-
-
     }
 }
