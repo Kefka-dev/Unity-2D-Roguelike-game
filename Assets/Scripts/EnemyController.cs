fdf448284ec9b82eca2af5f683948cb508f9b60e
Kefka-dev <sith.patri@gmail.com>
2021-11-09 17:46:26 +0100

    organized variables

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,23 +6,30 @@ public class EnemyController : MonoBehaviour
 {
     public Rigidbody2D theRB;
     public float moveSpeed;
-    public Animator anim;
-    public int health;
-    public int giveTouchDamage;
+
     public float rangeToChasePlayer;
+    private Vector3 moveDirection;
+
+    public Animator anim;
+
+    public int health;
+
     public GameObject[] deatchSplatters;
     public GameObject hitEffect;
-    
+
     public bool shouldShoot;
+
     public GameObject bullet;
     public Transform firePoint;
     public float fireRate;
+    private float fireCounter;
+
+
+    public int giveTouchDamage;
+
     public float shootRange;
 
     public SpriteRenderer theBody;
-
-    private Vector3 moveDirection;
-    private float fireCounter;
 
     // Start is called before the first frame update
     void Start()
