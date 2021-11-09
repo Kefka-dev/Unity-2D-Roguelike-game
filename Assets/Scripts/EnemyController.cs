32836b223f9200674e4ddb2f4f54be47eb417adb
Kefka-dev <sith.patri@gmail.com>
2021-11-09 18:43:58 +0100

    added new enemy - wandering enemy, slime

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -7,13 +7,20 @@ public class EnemyController : MonoBehaviour
     public Rigidbody2D theRB;
     public float moveSpeed;
 
+    //chasing enemy
     public bool shouldChase;
     public float rangeToChasePlayer;
     private Vector3 moveDirection;
 
-
+    //runaway enemy
     public bool shouldRunAway;
     public float runawayRange;
+
+    //wanderring enemy
+    public bool shouldWander;
+    public float wanderLenght, pauseLenght;
+    private float wanderCounter, pauseCounter;
+    private Vector3 wanderDirection;
 
     public Animator anim;
 
@@ -39,7 +46,10 @@ public class EnemyController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (shouldWander)
+        {
+            pauseCounter = Random.Range(pauseLenght * .75f, pauseLenght * 1.25f);
+        }
     }
 
     // Update is called once per frame
@@ -54,6 +64,37 @@ public class EnemyController : MonoBehaviour
             {
                 moveDirection = PlayerController.instance.transform.position - transform.position;
                 anim.SetBool("isMoving", true);
+            } else
+            {
+                if (shouldWander)
+                {
+                    if(wanderCounter > 0)
+                    {
+                        wanderCounter -= Time.deltaTime;
+
+                        //move enemy
+                        moveDirection = wanderDirection;
+
+                        if(wanderCounter <= 0)
+                        {
+                            pauseCounter = Random.Range(pauseLenght * .75f, pauseLenght * 1.25f);
+                        }
+
+                    }
+
+                    if(pauseCounter > 0)
+                    {
+                        pauseCounter -= Time.deltaTime;
+                        if(pauseCounter <= 0)
+                        {
+                            wanderCounter = Random.Range(wanderLenght * .75f, wanderLenght * 1.25f);
+
+                            wanderDirection = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+                            anim.SetBool("isMoving", true);
+                        }
+                    }
+
+                }
             }
 
             if(shouldRunAway && Vector3.Distance(transform.position, PlayerController.instance.transform.position) < runawayRange)
@@ -61,6 +102,7 @@ public class EnemyController : MonoBehaviour
                 moveDirection = transform.position - PlayerController.instance.transform.position;
                 anim.SetBool("isMoving", true);
             }
+
             /*else
             {
                 moveDirection = Vector3.zero;
