75f0ae71bc23079f58033af5bb6cc363a615d8a5
Kefka-dev <sith.patri@gmail.com>
2022-03-28 19:11:05 +0200

    finalfinal

--- Assets/Scripts/ShopItem.cs
+++ Assets/Scripts/ShopItem.cs
@@ -44,42 +44,54 @@ public class ShopItem : MonoBehaviour
             {
                 if(LevelManager.instance.currentCoins >= itemPrice)
                 {
-                    LevelManager.instance.spendCoins(itemPrice);
 
-
-                    if(isHealthRestore)
+                    if(isHealthRestore && PlayerHealthController.instance.currentHealth!=PlayerHealthController.instance.maxHealth)
                     {
+                        LevelManager.instance.spendCoins(itemPrice);
                         PlayerHealthController.instance.HealPlayer(HealAmmount);
                         Destroy(gameObject);
                     }
 
                     if (isHealthUpgrade)
                     {
+                        LevelManager.instance.spendCoins(itemPrice);
                         PlayerHealthController.instance.increaseMaxHealth(MaxHealthIncreaseAmount);
                         Destroy(gameObject);
                     }
-
+                    
                     if (isWeapon)
                     {
-                        Gun gunClone = Instantiate(theGun);
-                        gunClone.transform.parent = PlayerController.instance.gunArm;
-                        if (gunClone.Minigun == true)
+                        bool hasGun = false;
+                        foreach (Gun gunToCheck in PlayerController.instance.availableGuns)
                         {
-                            gunClone.transform.position = new Vector3(PlayerController.instance.gunArm.position.x + .1f, PlayerController.instance.gunArm.position.y - .4f, PlayerController.instance.gunArm.position.z);
-                            Debug.Log("MAM MINIGUN");
+                            if (theGun.weaponName == gunToCheck.weaponName)
+                            {
+                                hasGun = true;
+                            }
                         }
-                        else
+                        if (hasGun == false)
                         {
-                            gunClone.transform.position = PlayerController.instance.gunArm.position;
-                            Debug.Log("MAM INU ZBRAN");
-                        }
-                        gunClone.transform.localRotation = Quaternion.Euler(Vector3.zero);
-                        gunClone.transform.localScale = Vector3.one;
+                            LevelManager.instance.spendCoins(itemPrice);
+                            Gun gunClone = Instantiate(theGun);
+                            gunClone.transform.parent = PlayerController.instance.gunArm;
+                            if (gunClone.Minigun == true)
+                            {
+                                gunClone.transform.position = new Vector3(PlayerController.instance.gunArm.position.x + .1f, PlayerController.instance.gunArm.position.y - .4f, PlayerController.instance.gunArm.position.z);
+                                Debug.Log("MAM MINIGUN");
+                            }
+                            else
+                            {
+                                gunClone.transform.position = PlayerController.instance.gunArm.position;
+                                Debug.Log("MAM INU ZBRAN");
+                            }
+                            gunClone.transform.localRotation = Quaternion.Euler(Vector3.zero);
+                            gunClone.transform.localScale = Vector3.one;
 
-                        PlayerController.instance.availableGuns.Add(gunClone);
-                        PlayerController.instance.currentGun = PlayerController.instance.availableGuns.Count - 1;
-                        PlayerController.instance.switchGun();
-                        Destroy(gameObject);
+                            PlayerController.instance.availableGuns.Add(gunClone);
+                            PlayerController.instance.currentGun = PlayerController.instance.availableGuns.Count - 1;
+                            PlayerController.instance.switchGun();
+                            Destroy(gameObject);
+                        }
                     }
 
                     AudioManager.instance.PlaySFX(19);
